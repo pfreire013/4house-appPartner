604485444fc6fa62e38103f44928392b53c04eb8
José Freire <contato.pt.paulofreire@gmail.com>
2020-12-03 21:00:28 +0000

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {
+  Text
+} from 'react-native'
+
+const App = () => {
+  return (
+    <>
+      <Text>4house Partner</Text>
+    </>
+  )
+}
+
+export default App
