7cf7362f88aadfa03e3c0c6ee56a5a2f9d93bfbd
José Freire <contato.pt.paulofreire@gmail.com>
2020-12-04 01:34:16 +0000

    Create base theme styles

---
+++ src/theme.ts
@@ -0,0 +1,19 @@
+const theme = {
+  color: {
+    primary: '#3574F2',
+    secondary: '#FF5B44',
+    white: '#FFFFFF',
+    black: '#000000',
+    lowGrey: '#F2F2F7',
+    mediumGrey: '#C7C7CC',
+    highGrey: '#8E8E93',
+    success: '#A4E786',
+    green: '#34C759',
+    red: '#FF3B30'
+  },
+  fontFamily: {
+    Roboto: 'Roboto'
+  }
+}
+
+export default theme
